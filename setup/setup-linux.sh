b4de73dce33674e2e3468afe26e836da8a08c62b
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2023-08-13 16:44:02 +0200

    Adds broot cmdline util

--- setup/setup-linux.sh
+++ setup/setup-linux.sh
@@ -48,3 +48,6 @@ wget https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-
     -O /tmp/bat_${DELTA_VERSION}_${ARCHTYPE}.deb && \
 dpkg -i /tmp/bat_${DELTA_VERSION}_${ARCHTYPE}.deb && \
 rm /tmp/bat_${DELTA_VERSION}_${ARCHTYPE}.deb
+
+# install broot
+wget https://dystroy.org/broot/download/x86_64-linux/broot && mv broot /usr/local/bin
