7ee98b0963f60ce122db8202210d14dbedaac27a
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2025-04-18 14:26:42 +0000

    Fix

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -5,30 +5,35 @@ title "Automatic installation of custom packages for Windows"
 cls
 
 echo install chocolatey from the internet...
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass ^
--Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" ^
-&& SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+rem echo install chocolatey from the internet...
+rem @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass ^
+rem -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" ^
+rem && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 rem echo install packages with chocolatey...
 rem choco install -y cygwin cyg-get nmap virtualbox vagrant nodepadplusplus putty javaruntime caffeine
 
 echo install generic packages with chocolatey...
-choco install -y nodepadplusplus firefox paint.net vlc googlechrome adobereader 7zip
-choco install -y winscp wireshark filezilla
+choco install -y firefox paint.net vlc googlechrome adobereader 7zip
+rem choco install -y notepadplusplus
+choco install -y winscp filezilla
+rem choco install -y wireshark
 choco install -y wget curl git openssh dbeaver kubernetes-cli
-choco install -y dotnetcore-sdk dotnet-5.0-sdk
 choco install -y oraclejdk maven
 choco install -y nodejs
-choco install -y ngrok
+rem choco install -y ngrok
 choco install -y screenpresso
 choco install -y soundswitch
 choco install -y autohotkey
 choco install -y eraser
 choco install -y bruno
+choco install -y docker-desktop
 rem choco install -y caffeine
 
 echo install microsoft-specific packages with chocolatey...
-choco install -y sysinternals vscode microsoftazurestorageexplorer azure-cli sql-server-management-studio dotnetcore-sdk powerbi visualstudio2019enterprise gh powershell-core
+choco install -y sysinternals vscode microsoftazurestorageexplorer azure-cli sql-server-management-studio dotnetcore-sdk powerbi visualstudio2019enterprise gh powershell-core dotnet-8.0-sdk
 choco install -y zoomit
 
 echo install kubelogin
