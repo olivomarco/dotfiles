c694400bc1346e71c6e74d4f084d56a7780f9f48
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2025-04-22 08:10:30 +0000

    Update dotnet sdk

--- setup/setup-dev-linux.sh
+++ setup/setup-dev-linux.sh
@@ -10,10 +10,10 @@ if [ $EUID -ne 0 ] ; then
 fi
 
 # install dotnet sdk
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-apt-get update; apt-get install -y apt-transport-https && apt-get update && apt-get install -y aspnetcore-runtime-5.0
+apt-get update; apt-get install -y apt-transport-https && apt-get update && apt-get install -y dotnet-sdk-9.0 aspnetcore-runtime-9.0
 
 # install java stuff
 apt-get install -y default-jdk maven
