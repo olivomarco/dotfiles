9b30d02bc9d5a272abb71734052283ad431088d2
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2025-04-22 10:25:45 +0000

    Fix packages

--- setup/setup-k8s-linux.sh
+++ setup/setup-k8s-linux.sh
@@ -10,11 +10,11 @@ if [ $EUID -ne 0 ] ; then
 fi
 
 # install kubernetes stuff
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+#deb http://apt.kubernetes.io/ kubernetes-xenial main
+#EOF
+#apt-get update
 apt-get install -y kubectl
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
